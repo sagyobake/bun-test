382faeeda9a2fdb29832e6c13b875b53bfdcf750
sagyobake <sagyobake@localhost.localdomain>
2025-02-27 11:30:58 +0900

    upgrade

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // WebSocket 接続を作成
 //const socket = new WebSocket("ws://localhost:3000");
-const socket = new WebSocket("wss://bun-test-1.onrender.com");
+const socket = new WebSocket("wss://bun-test-ten.vercel.app/");
 
 // 接続が開いたときのイベント
 socket.addEventListener("open", (event) => {
